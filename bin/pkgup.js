35e3d01e7b6afb38f529c0b5b420e8403ce6110d
Nicolas Wan <nbwansjtu@gmail.com>
2019-07-15 15:23:31 +0800

    feat: logger & argv yargs processing

--- bin/pkgup.js
+++ bin/pkgup.js
@@ -3,5 +3,11 @@
 'use strict';
 
 const command = require('..');
+const logger = require('../lib/logger');
 
-command.run(process.argv.slice(2));
+command
+  .run(process.argv.slice(2))
+  .catch((err) => {
+    logger.error(err.stack);
+    process.exit(1);
+  });
